d79e0832cfd76fe0fb0b8af4f8ca7eae4bbd53d8
Azamat2007Mlove <106876664+Azamat2007Mlove@users.noreply.github.com>
2024-02-03 16:29:20 +0500

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <>
+    <aside>
+        <img src="/form.png" alt="" />
+    </aside>
+    <footer>
+        <img src="/footer.png" alt="" />
+    </footer>
+    </>
+  )
+}
+
+export default Footer
